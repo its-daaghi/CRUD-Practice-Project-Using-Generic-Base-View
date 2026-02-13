3a5087bee58b5c6246d52f641311967ceb73e398
its-daaghi <1muhammadahmad0313@gmail.com>
2026-02-13 15:09:48 +0500

    Add Detail view

--- CRUD_Generic/myapp/urls.py
+++ CRUD_Generic/myapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import StudentCreateView, StudentListView
+from .views import StudentCreateView, StudentListView, StudentDetailView
 
 
 urlpatterns = [
     path("", StudentListView.as_view(), name="student_list"),
     path("student/add/", StudentCreateView.as_view(), name="student_add"),
+    path("student/<int:pk>/", StudentDetailView.as_view(), name="student_detail"),
 ]
--- CRUD_Generic/myapp/views.py
+++ CRUD_Generic/myapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.views.generic import (
+    CreateView,
+    ListView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Student
 from django.urls import reverse_lazy
 
@@ -18,3 +24,9 @@ class StudentListView(ListView):
     model = Student
     template_name = "myapp/student_list.html"
     context_object_name = "students"
+
+
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = "myapp/student_detail.html"
+    context_object_name = "student"
